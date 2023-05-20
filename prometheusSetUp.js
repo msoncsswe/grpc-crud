ed2f2cad77c1c4d9720723294627d1c69d0756f5
msoncsswe <miris11260@gmail.com>
2023-05-20 12:05:38 -0400

    commiting file containing prometheus setup

---
+++ prometheusSetUp.js
@@ -0,0 +1,18 @@
+//require prom-client: prometheus for node.js that supports historgram, summaries, gauges and counters
+//works with nice-grpc-prometheus
+const promClient = require('prom-client');
+//globalRegistry is assinged to an instance of Registery
+const globalRegistry = promClient.register;
+//Registry is the class in prom-client (lambda Registry)
+const {Registry} = promClient;
+//console.log(`globalRegistry: ${globalRegistry}, Registry: ${Registry}`)
+
+//require nice-grpc-prometheus
+const niceGrpcClient = require('nice-grpc-prometheus');
+const niceGrpcRegistry = niceGrpcClient.registry;
+
+//merge niceGrpcRegistry with global (use await to export all metrics)
+const mergedRegistry = Registry.merge([globalRegistry, niceGrpcRegistry]);
+
+//export
+module.exports = mergedRegistry;
