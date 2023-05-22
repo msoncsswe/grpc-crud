78b70812436a8848b070714bf917479365ea8d05
msoncsswe <miris11260@gmail.com>
2023-05-22 13:57:42 -0400

    adding grpc file

---
+++ @grpc/client.js
@@ -0,0 +1,43 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_FILE = './proto/contacts.proto';
+
+const pkgDef = protoLoader.loadSync(PROTO_FILE, {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    dafaults: true,
+    onesofs: true,
+});
+
+const ContactService = grpc.loadPackageDefinition(pkgDef).ContactService;
+
+const client = new ContactService("localhost:3500", grpc.credentials.createInsecure());
+
+const request = {firstName: 'JOhnny', lastName: 'Ngyuen'};
+
+client.GetContact(request, (err, contact) => {
+    try{
+        console.log('here is the contact: ', contact)
+    }
+    catch(err) {
+        console.log(err)
+    }
+})
+
+client.GetContactList({}, async (err, contact) => {
+    try{
+        console.log('this is the list:', contact.contacts)
+    }
+    catch (err){
+        console.log(err)
+    }
+})
+
+client.CreateContact({firstName: 'mel', lastName: 'Ngyuen', telNum: 1234567890, email: 'patNgyuen@aol.com'}, (err, contact) => {
+    try {
+        console.log('contact created')
+    } catch(err) {
+        console.log(err)
+    }
+})
---
+++ @grpc/server.js
@@ -0,0 +1,60 @@
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const PROTO_FILE = './proto/contacts.proto';
+const Contacts = require('../models/contactModel');
+const contactImpl = require('./contactImpl');
+
+const options = {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,   
+};
+
+const packageDef = protoLoader.loadSync(PROTO_FILE, options);
+
+const contactsProto = grpc.loadPackageDefinition(packageDef);
+
+//this function gets one contact
+const getContact = async (call, callback) => {
+    console.log('trying to get contact')
+    try{
+        const response =  await contactImpl.getContactDB(call.request)
+        console.log('back in server with data', response)
+        await callback(null, response);
+        }
+
+    catch(err){
+        console.log(err, 'got an error')
+    }
+}
+
+//this function gets all of the contacts
+const getContactList = async (call, callback) => {
+        try{
+            const response = await contactImpl.getContactList({})
+            console.log('this is response', response)
+            callback(null, response)
+        }
+        catch(err){
+            console.log(err, 'this is an error bish')
+        }
+}
+
+const createContact = (call, callback) => {
+    console.log(`createContact invoked`)
+    const res = contactImpl.addContactDB(call.request);
+    try {
+        callback(null, res)
+    } catch(err) {
+        console.log(`error in createContact:`, err)
+    }
+}
+
+grpcServer.addService(contactsProto.ContactService.service, {GetContact: getContact, GetContactList: getContactList, CreateContact: createContact});
+
+grpcServer.bindAsync("127.0.0.1:3500", grpc.ServerCredentials.createInsecure(), (err, port) => {
+    console.log(`listening on port ${port}`);
+    grpcServer.start()
+})
